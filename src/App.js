b53eea277dc0ca3f38d68c45d6314f32d1557f34
AJ197028 <aksjais20@gmail.com>
2023-05-06 04:44:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Fragment, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import DummyScreen from './Components/Layout/DummyScreen';
+import Navbar from './Components/Layout/Navbar'
+import ProfileDetails from './Components/Layout/ProfileDetails';
+import Login from './Components/Pages/Login';
+import ForgetPasswordPage from './Components/Pages/ForgetPasswordPage';
+import Expenses from './Components/Pages/Expenses';
+import { expenseActions } from './Components/Store';
+
+function App() {
+  const token = useSelector(state => state.authentication.token);
+  const isLoggedIn = useSelector(state => state.authentication.isLoggedIn);
+  //let ctx = useContext(expContext);
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route exact path='/forget'>
+          <ForgetPasswordPage />
+        </Route>
+        <Route exact path='/profile'>
+          {token && <DummyScreen />}
+          {!token && <Redirect to='/' />}
+        </Route>
+        <Route exact path='/details'>
+          <ProfileDetails />
+        </Route>
+        <Route exact path='/expenses'>
+          {isLoggedIn && <Expenses />}
+          {!isLoggedIn && <Redirect to='/' />}
+        </Route>
+      </Switch>
+    </Fragment>
+  );
+}
+
+export default App;
